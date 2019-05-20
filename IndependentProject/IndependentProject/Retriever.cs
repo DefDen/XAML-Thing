1d4efde64dd72e188eb2792362a126e9c668e526
DefDen <31528205+DefDen@users.noreply.github.com>
2019-05-20 09:49:26 -0700

    Finished MusixMatch API Retrieval Methods
    Using the previous API retrieval method I made for the track searching, made a method that gets the lyrics of a song given a track id.

--- IndependentProject/IndependentProject/Retriever.cs
+++ IndependentProject/IndependentProject/Retriever.cs
@@ -13,14 +13,35 @@ namespace IndependentProject
     {
         private string musixMatchKey = "2eb5dcbf3889a59f9a6ff077176d906a";
 
-        public async Task<MusicSearchRootObject> GetTrackSearchResults(string track)
+        public async Task<MusicSearchRootObject> GetTrackSearchResults(string term, bool isTrackSearch)
         {  
             HttpClient httpClient = new HttpClient();
-            String apiUrl = $"http://api.musixmatch.com/ws/1.1/track.search?q_track={track}&apikey={musixMatchKey}";
+            String apiUrl = "";
+
+            if(isTrackSearch)
+            {
+                apiUrl = $"http://api.musixmatch.com/ws/1.1/track.search?q_track={term}&apikey={musixMatchKey}";
+            }
+            else
+            {
+                apiUrl = $"http://api.musixmatch.com/ws/1.1/track.search?q_artist={term}&apikey={musixMatchKey}";
+            }
 
             string responeString = await httpClient.GetStringAsync(apiUrl);
 
             MusicSearchRootObject results = JsonConvert.DeserializeObject<MusicSearchRootObject>(responeString);
+
+            return results;
+        }
+        
+        public async Task<LyricsRootObject> GetLyrics(string trackId)
+        {
+            HttpClient httpClient = new HttpClient();
+            String apiUrl = $"https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id={trackId}&apikey={musixMatchKey}";
+
+            string responseString = await httpClient.GetStringAsync(apiUrl);
+
+            LyricsRootObject results = JsonConvert.DeserializeObject<LyricsRootObject>(responseString);
 
             return results;
         }
